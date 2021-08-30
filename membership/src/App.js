633f549c0394d30b14e67dcf132e1deb2e370b1b
Krsto Kostic <kostic.krsto@gmail.com>
2021-08-31 01:21:35 +0200

    addinf project

---
+++ membership/src/App.js
@@ -0,0 +1,39 @@
+import { MuiThemeProvider, createTheme } from "@material-ui/styles";
+import {
+    indigo,
+    pink
+} from "@material-ui/core/colors";
+import { BrowserRouter } from "react-router-dom";
+import MainRouter from "./MainRouter";
+
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            light: "#757de8",
+            main: "#3f51b5",
+            dark: "#002984",
+            contrastText: "#fff",
+        },
+        secondary: {
+            light: "#ff79b0",
+            main: "#ff4081",
+            dark: "#c60055",
+            contrastText: "#000",
+        },
+        openTitle: indigo["400"],
+        protectedTitle: pink["400"],
+        type: "light"
+    }
+})
+
+
+const App = () => {
+    <BrowserRouter>
+        <MuiThemeProvider theme={theme}>
+            <MainRouter />
+        </MuiThemeProvider>
+    </BrowserRouter>
+}
+
+export default App;
